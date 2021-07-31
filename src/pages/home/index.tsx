34dbce3cb31d45a0ff05b0e5991a2bb515158e05
jhonscarpa <scarpa.jhonatan@outlook.com>
2021-07-31 14:42:52 -0300

    nav

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,76 @@
+import React, { useState } from 'react'
 import homeImg from '../../assets/homeImg.jpg'
+import Container from './styles'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 
 export function Home() {
-  return (
-    <div className="home">
-      <div>
-        <img src={homeImg} alt="cabelo loiro e cacheado" />
-      </div>
+  const [showMenu, setShowMenu] = useState<boolean>(false)
 
-      <div>
-        <h1>Saúde natural para os seus cabelos</h1>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-          eligendi, at maiores aperiam, porro excepturi fuga magni magnam eaque
-          odit nesciunt. Quidem iusto aperiam, sunt quod autem corporis unde
-          eaque.
-        </p>
+  return (
+    <Container>
+      {showMenu ? (
+        <AiOutlineClose className="icon" onClick={() => setShowMenu(false)} />
+      ) : (
+        <AiOutlineMenu className="icon" onClick={() => setShowMenu(true)} />
+      )}
+
+      <header id="header">
+        <nav className="container">
+          <a className="logo" href="#">
+            Stillus&amp;<span>Beleza</span>.
+          </a>
+          {showMenu && (
+            <div className="menu">
+              <ul className="grid">
+                <li>
+                  <a className="title" href="#home">
+                    Início
+                  </a>
+                </li>
+                <li>
+                  <a className="title" href="#about">
+                    sobre
+                  </a>
+                </li>
+                <li>
+                  <a className="title" href="#services">
+                    Serviços
+                  </a>
+                </li>
+                <li>
+                  <a className="title" href="#galery">
+                    Galeria
+                  </a>
+                </li>
+                <li>
+                  <a className="title" href="#scheduling">
+                    Agendamento
+                  </a>
+                </li>
+                <li>
+                  <a className="title" href="#contact">
+                    Contato
+                  </a>
+                </li>
+              </ul>
+            </div>
+          )}
+        </nav>
+      </header>
+
+      <div id="home">
+        <div>
+          <img src={homeImg} alt="cabelo preto" />
+        </div>
+
+        <div>
+          <h1>Saúde natural para os seus cabelos</h1>
+          <p>
+            Um salão exclusivo em São Paulo, especializado em tratamentos
+            naturais.
+          </p>
+        </div>
       </div>
-    </div>
+    </Container>
   )
 }
---
+++ src/pages/home/styles.ts
@@ -0,0 +1,24 @@
+import styled  from 'styled-components'
+
+const Container = styled.div`
+font-family: 'Times New Roman', Times, serif ;
+
+#home{
+
+  width: 100%;
+ 
+img {
+  border-radius: 4px;
+  height: 400px;
+  
+
+}
+}
+
+.icon{
+  color: #875afd;
+  cursor: pointer;
+}
+`
+
+export default Container
