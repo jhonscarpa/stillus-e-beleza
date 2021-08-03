0347d321499b8725828b1891eb187a160dfddb0d
jhonscarpa <scarpa.jhonatan@outlook.com>
2021-08-03 14:29:34 -0300

    start styled homepag

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import homeImg from '../../assets/homeImg.jpg'
-import Container from './styles'
+import { Container, Nav, Title, Section } from './styles'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 
 export function Home() {
@@ -8,17 +8,20 @@ export function Home() {
 
   return (
     <Container>
-      {showMenu ? (
-        <AiOutlineClose className="icon" onClick={() => setShowMenu(false)} />
-      ) : (
-        <AiOutlineMenu className="icon" onClick={() => setShowMenu(true)} />
-      )}
-
       <header id="header">
-        <nav className="container">
+        <Nav className="box">
           <a className="logo" href="#">
-            Stillus&amp;<span>Beleza</span>.
+            Stillus&amp;<span>Beleza</span>
           </a>
+          {showMenu ? (
+            <AiOutlineClose
+              className="icon"
+              onClick={() => setShowMenu(false)}
+            />
+          ) : (
+            <AiOutlineMenu className="icon" onClick={() => setShowMenu(true)} />
+          )}
+
           {showMenu && (
             <div className="menu">
               <ul className="grid">
@@ -55,22 +58,33 @@ export function Home() {
               </ul>
             </div>
           )}
-        </nav>
+        </Nav>
       </header>
 
-      <div id="home">
-        <div>
-          <img src={homeImg} alt="cabelo preto" />
-        </div>
+      <Section id="home">
+        <div className="box grid">
+          <div className="image">
+            <img src={homeImg} alt="cabelo preto" />
+          </div>
 
-        <div>
-          <h1>Saúde natural para os seus cabelos</h1>
-          <p>
-            Um salão exclusivo em São Paulo, especializado em tratamentos
-            naturais.
-          </p>
+          <div className="text">
+            <Title> Saúde natural para os seus cabelos</Title>
+            <p>
+              Um salão exclusivo em São Paulo, especializado em tratamentos
+              naturais.
+            </p>
+            <a href="#" className="button">
+              Agendar um horário
+            </a>
+          </div>
         </div>
-      </div>
+      </Section>
+
+      <Section id="about">
+        <div className="box">
+          <Title>Sobre</Title>
+        </div>
+      </Section>
     </Container>
   )
 }
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,24 +1,120 @@
-import styled  from 'styled-components'
+import styled from 'styled-components'
 
-const Container = styled.div`
-font-family: 'Times New Roman', Times, serif ;
-
-#home{
-
-  width: 100%;
- 
-img {
-  border-radius: 4px;
-  height: 400px;
-  
-
-}
-}
-
-.icon{
+export const Title = styled.h1`
+  font: 700 1.875rem 'Poppins', sans-serif;
+  text-align: center;
   color: #875afd;
-  cursor: pointer;
-}
+  -webkit-font-smoothing: auto;
 `
 
-export default Container
+export const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 4.5rem;
+
+  .menu {
+    width: 100%;
+    justify-content: center;
+  }
+
+  .logo {
+    font: 700 1.31rem 'Poppins', sans-serif;
+    color: #120310;
+
+    & span {
+      color: #875afd;
+    }
+  }
+
+  ul li a {
+    color: black;
+
+    &:hover {
+      transition: 0.3s;
+      color: #875afd;
+    }
+  }
+`
+
+export const Container = styled.div`
+  font: 400 1rem 'DM Sans';
+  background: white;
+  color: #454444;
+  -webkit-font-smoothing: antialiased;
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  img {
+    width: 100% 0;
+    height: auto;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  .grid {
+    display: grid;
+  }
+
+  .box {
+    width: 100%;
+    margin-left: 1.5rem;
+    margin-right: 1.5rem;
+  }
+
+  header {
+    border-bottom: 1px solid #e4e4e4;
+    margin-bottom: 2rem;
+    display: flex;
+
+    width: 100%;
+    position: fixed;
+    z-index: 100;
+    top: 0;
+    left: 0;
+    background-color: hsl(0 0% 98%);
+
+    .icon {
+      color: #875afd;
+      cursor: pointer;
+      align-items: center;
+      justify-content: space-between;
+    }
+  }
+
+  #home {
+    img {
+      border-radius: 4px;
+      max-height: 25rem;
+      margin: 0;
+    }
+
+    .button {
+      font: 500 1rem 'DM Sans', sans-serif;
+      background-color: #875afd;
+      color: white;
+      border-radius: 4px;
+
+      display: flex;
+      padding: 0.875rem 2rem 0.9375rem;
+      width: 13.375rem;
+      height: 3.5rem;
+      left: 5.06rem;
+      top: 31.812rem;
+    }
+  }
+`
+
+export const Section = styled.section`
+  padding: 5rem 0;
+`
